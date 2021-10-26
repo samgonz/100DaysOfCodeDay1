b384fe31250568911f23f1a7d2be1d33d63b1541
samgonzales <doomteam1@gmail.com>
2021-10-25 23:15:29 -0700

    Completed day 1 of 100 days of code

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+username = input("Hello User, What is your name?\n")
+print(f'Wow! {username}! I am from there as well.')
+user_born_city = input(f'What city were you born in {username}?\n')
+print(f'Amazing, if i had to give you a band name it would be,\n{username} {user_born_city}!')
