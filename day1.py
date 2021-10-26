36629aafc26f2379e34274a1e68dcb0a7a842c27
samgonzales <doomteam1@gmail.com>
2021-10-25 23:19:53 -0700

    Completed day 1 of 100 days of code

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
 username = input("Hello User, What is your name?\n")
-print(f'Wow! {username}! I am from there as well.')
-user_born_city = input(f'What city were you born in {username}?\n')
-print(f'Amazing, if i had to give you a band name it would be,\n{username} {user_born_city}!')
+print(f'Wow! {username.capitalize()}! I am from there as well.')
+user_born_city = input(f'What city were you born in {username.capitalize()}?\n')
+print(f'Amazing, If I had to give you a band name it would be,\n{username.capitalize()} {user_born_city.capitalize()}!')
--- main.py
+++
@@ -1,4 +0,0 @@
-username = input("Hello User, What is your name?\n")
-print(f'Wow! {username}! I am from there as well.')
-user_born_city = input(f'What city were you born in {username}?\n')
-print(f'Amazing, if i had to give you a band name it would be,\n{username} {user_born_city}!')
