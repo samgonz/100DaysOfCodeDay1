f18537f0fc072758f08b538f2d4563fad55d6def
samgonzales <doomteam1@gmail.com>
2021-10-25 23:16:42 -0700

    Completed day 1 of 100 days of code

---
+++ day1.py
@@ -0,0 +1,4 @@
+username = input("Hello User, What is your name?\n")
+print(f'Wow! {username}! I am from there as well.')
+user_born_city = input(f'What city were you born in {username}?\n')
+print(f'Amazing, if i had to give you a band name it would be,\n{username} {user_born_city}!')
